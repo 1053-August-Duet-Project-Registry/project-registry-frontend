c1845b3a5918714db40826fa43effa9d1e7f7fad
gavinmoore2121 <76663367+gavinmoore2121@users.noreply.github.com>
2021-07-28 14:45:36 -0400

    lint specifications met, minor refactor on view-projects.component

--- src/app/components/iteration/iteration.component.ts
+++ src/app/components/iteration/iteration.component.ts
@@ -4,7 +4,7 @@ import { IterationService } from 'src/app/service/iteration.service';
 import { forkJoin, Subscription } from 'rxjs';
 import { Iteration } from 'src/app/models/iteration.model';
 import { Project } from 'src/app/models/project.model';
-​
+
 @Component({
   selector: 'app-iteration',
   templateUrl: './iteration.component.html',
@@ -12,17 +12,17 @@ import { Project } from 'src/app/models/project.model';
 })
 export class IterationComponent implements OnInit, OnDestroy {
 
-  //array of batchTemplates to put the 2 batch IDs into
+  // array of batchTemplates to put the 2 batch IDs into
   theBatches: BatchTemplate[] = [];
 
-  sub:Subscription = new Subscription(); // just for clean up memory purpose
+  sub: Subscription = new Subscription(); // just for clean up memory purpose
 
   // Send data to the parent component
   @Output() sendBatch: EventEmitter<BatchTemplate> = new EventEmitter<BatchTemplate>();
 
-  // Don't change this string value, it connected to the logic, the app will throw err. It's a placeholder/ first value for the selectBatch option
-  seletedIdAndBatchId : String = "Batches"
-​
+  // Don't change this string value, it connected to the logic, the app will throw err.
+  // It's a placeholder/ first value for the selectBatch option
+  seletedIdAndBatchId = 'Batches';
 constructor(private iterationService: IterationService) { }
 
 ngOnInit(): void {
@@ -31,23 +31,24 @@ ngOnInit(): void {
 
 // Clean up memory
 ngOnDestroy(): void{
-  this.sub.unsubscribe()
+  this.sub.unsubscribe();
 }
 
 selectBatch(){
   // skip placeholder value
-  if(this.seletedIdAndBatchId != "Batches"){
-    let separateBatchAndId = this.seletedIdAndBatchId.split("|");
+  if (this.seletedIdAndBatchId !== 'Batches'){
+    const separateBatchAndId = this.seletedIdAndBatchId.split('|');
     // id:number, batchId: string, skill:string,location:string, startDate:string, endDate:string
-    this.sendBatch.emit(new BatchTemplate(Number(separateBatchAndId[0]),separateBatchAndId[1],"","",separateBatchAndId[2],separateBatchAndId[3]));
+    this.sendBatch.emit(new BatchTemplate(Number(separateBatchAndId[0]), separateBatchAndId[1], '', '',
+      separateBatchAndId[2], separateBatchAndId[3]));
 
   }
-}​
+}
 
-  //make a call to the API to retrieve all batches
+  // make a call to the API to retrieve all batches
   httpGet(){
     // this.http.get(environment.gameDealApi+ "?id=" + i)
-      this.sub = this.iterationService.getBatchServiceMock().subscribe(batches => this.theBatches = batches)
+      this.sub = this.iterationService.getBatchServiceMock().subscribe(batches => this.theBatches = batches);
     /*let a;
       forkJoin([this.iterationService.getBatchServiceMock(), this.iterationService.getBatchService()]).subscribe(data => {
         //this.theBatches = data as batchTemplate[] // cast t
