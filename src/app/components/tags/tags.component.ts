d3dbc72d690854467210b292b15225984ea32e54
evanyeager128@gmail.com <evanyeager128@gmail.com>
2021-08-02 20:03:34 -0500

    fixed getAllTags() to get data from the db

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -87,20 +87,11 @@ export class TagsComponent implements OnInit {
   }
 
   getAllTags() {
-    // temporary hardcoding
-    this.tags = [new Tag(3, 'tag1', 'description', true)];
-    this.tagsNames = this.tags;
-
-
-      /*
-      * This block gets tags from the db
-      */
-    // this.tagService.getAllTags().subscribe(data => {
-    //   this.tags = data;
-    //   data.forEach(tag => {
-    //     this.tagsNames.push(tag);
-    //   });
-    // });
+    this.tagService.getAllTags().subscribe(data => {
+      this.tags = data;
+      this.selectedTagArr = data;
+      this.tagsNames = data;
+    });
   }
 
   private _filter(value: any): Tag[] {
